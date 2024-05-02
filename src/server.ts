047cd48b8ebf40c9cde4f754b29ce4035fd319f5
Henrique Reis <contatohenriquereis@gmail.com>
2024-05-02 17:37:27 -0300

    refactor: add database connection starts with server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
-import { app } from '../src/app'
-import 'dotenv/config'
+import { app } from "../src/app";
+import "dotenv/config";
+import mongoose from "mongoose";
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const stringConnection = process.env.DATABASE_URL as string;
 
 export const serverConnection = async () => {
-    app.listen(port, () => {
-        console.log(`Server running on port: ${port}`)
-    })
-}
+	const mongooseConnection = await mongoose.connect(stringConnection);
+	if (mongooseConnection) {
+		app.listen(port, () => {
+			console.log(`MongoDB Connected / Server running on port: ${port}`);
+		});
+	}
 
-serverConnection()
+	return mongooseConnection;
+};
+
+serverConnection();
