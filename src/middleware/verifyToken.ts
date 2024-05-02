efba2d075c0af819f2a78104e1205bb9c397b626
Henrique Reis <contatohenriquereis@gmail.com>
2024-05-02 11:02:56 -0300

    feat: verify token function middleware

---
+++ src/middleware/verifyToken.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+import { CustomException } from '../exceptions/customExceptions'
+
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    const secret = process.env.SECRET
+    const tokenHeader = req.headers.authorization
+
+    const token = tokenHeader && tokenHeader.split(' ')[1]
+
+    if (!token) {
+        return next(CustomException.UnathorizedException('Token Unauthorized'))
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, secret || '')
+        res.locals.token = decodedToken
+
+        next()
+    } catch (error) {
+        console.log('Error when executing token verification, invalid JWT signature')
+
+        next(error)
+    }
+}
