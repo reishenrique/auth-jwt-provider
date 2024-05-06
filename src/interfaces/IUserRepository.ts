c00cc3988eeaf570014615fd9cdd1a57ef0b923c
Henrique Reis <contatohenriquereis@gmail.com>
2024-05-06 16:48:16 -0300

    refactor: removing comment from linter

--- src/interfaces/IUserRepository.ts
+++ src/interfaces/IUserRepository.ts
@@ -2,6 +2,5 @@ import type { UserEntity } from "../entities/UserEntity";
 
 export interface IUserRepository {
 	create(user: UserEntity): Promise<object>;
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	findUserByEmail(email: string): Promise<any>;
 }
