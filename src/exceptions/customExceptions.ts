c7f68a988dd6dd218dfd1d385d5afad2cea9331b
Henrique Reis <contatohenriquereis@gmail.com>
2024-05-06 16:44:32 -0300

    feature: add not found custom exception

--- src/exceptions/customExceptions.ts
+++ src/exceptions/customExceptions.ts
@@ -1,17 +1,21 @@
-import { StatusCodes } from 'http-status-codes'
+import { StatusCodes } from "http-status-codes";
 
 export class CustomException extends Error {
-    public readonly statusCode: number
-    constructor(message: string, statusCode: number) {
-        super(message)
-        this.statusCode = statusCode
-    }
+	public readonly statusCode: number;
+	constructor(message: string, statusCode: number) {
+		super(message);
+		this.statusCode = statusCode;
+	}
 
-    static UnauthorizedException(message: string): CustomException {
-        return new CustomException(message, StatusCodes.UNAUTHORIZED)
-    }
+	static UnauthorizedException(message: string): CustomException {
+		return new CustomException(message, StatusCodes.UNAUTHORIZED);
+	}
 
-    static ConflictException(message: string): CustomException {
-        return new CustomException(message, StatusCodes.CONFLICT)
-    }
+	static ConflictException(message: string): CustomException {
+		return new CustomException(message, StatusCodes.CONFLICT);
+	}
+
+	static NotFoundException(message: string): CustomException {
+		return new CustomException(message, StatusCodes.NOT_FOUND);
+	}
 }
