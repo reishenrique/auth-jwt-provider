6a1db356d44e713a9af93cafbe3611e868a849e1
Henrique Reis <contatohenriquereis@gmail.com>
2024-05-20 12:00:51 -0300

    test: throwing ax exception when trying to register the same email twice

--- src/tests/unit/authService.spec.ts
+++ src/tests/unit/authService.spec.ts
@@ -1,3 +1,4 @@
+import { CustomException } from "../../exceptions/customExceptions";
 import { IUserRepository } from "../../interfaces/IUserRepository";
 import { AuthService } from "../../services/authService";
 import { authRepositoryInMemory } from "../mock/userRepositoryInMemory";
@@ -34,5 +35,22 @@ describe("Auth Service unit tests", () => {
         
         expect(mockUserRepository.create).toHaveBeenCalledTimes(1)
         expect(mockUserRepository.findUserByEmail).toHaveBeenCalledTimes(1)
+    })
+
+    it("Should throw an exception when email already registered in the system", async () => {
+        const user = {
+            userName: "John Doe",
+            email: "test@email.com",
+            password: "testpassword@123"
+        }
+
+        const { sut, mockUserRepository } = makeSut([user])
+
+        expect(sut.signUp(user)).rejects.toThrow(
+            CustomException.ConflictException("E-mail already registered")
+        )
+
+        expect(mockUserRepository.findUserByEmail).toHaveBeenCalledTimes(1)
+        expect(mockUserRepository.create).not.toHaveBeenCalled
     })
 })
